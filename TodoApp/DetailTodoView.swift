eea8307a8a53ba3235c65c80b1c1620c6eb44fec
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-06-21 14:22:40 +0200

    add blank todo detail

---
+++ TodoApp/DetailTodoView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailTodoView.swift
+//  TodoApp
+//
+//  Created by Adam Buryšek on 21.06.2024.
+//
+
+import SwiftUI
+
+struct DetailTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailTodoView()
+}
